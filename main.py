23fa47da7ac60701d7cd280cfb0b3fcc59bcfa48
imerla1 <giorgi1048@yahoo.com>
2020-09-11 16:16:53 +0400

    ff commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+import os
+
+app = Flask(__name__)
+passwd = '1234'
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/restart')
+def restart():
+    os.system()
+    return "Restart Finished"
+
+@app.route('/shutdown')
+def shutdown():
+    return 'SHUTDOWN'
+
+app.run(debug=True)
